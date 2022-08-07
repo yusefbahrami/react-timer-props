a8b17679a1d2ce821eacffb0979426da1d9bdb4f
ALAEDDIN <94550523+yusefbahrami@users.noreply.github.com>
2022-08-07 15:25:55 +0430

    completed

--- src/Timer.js
+++ src/Timer.js
@@ -1,12 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+var interval;
 class Timer extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      second: 60,
+    };
+  }
+  startIntervalFunction = () => {
+    interval = setInterval(() => {
+      this.setState({
+        second: this.state.second - 1,
+      });
+    }, 1000);
+  };
+
+  stopIntervalFunction = () => {
+    clearInterval(interval);
+  };
+
+  componentDidMount() {
+    this.startIntervalFunction();
+  }
+  componentDidUpdate() {
+    if (this.state.second === 0) {
+      this.stopIntervalFunction();
+    }
+  }
   render() {
     return (
-      <div className="btns">
-        <button onClick={this.props.startInterval}>Start</button>
-        <button onClick={this.props.stopInterval}>Stop</button>
-      </div>
+      <>
+        <h2 className="timer">{this.state.second}</h2>
+        <div className="btns">
+          <button onClick={this.startIntervalFunction}>Start</button>
+          <button onClick={this.stopIntervalFunction}>Stop</button>
+        </div>
+      </>
     );
   }
 }
